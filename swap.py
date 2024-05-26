58e9ae24c35ef84a8f8fc143d721343464d90bed
karolina <kitanovska.karolina@yahoo.com>
2024-05-26 23:36:58 +0200

    commit 2

---
+++ swap.py
@@ -0,0 +1,19 @@
+def s(a, b):
+    def number_to_string(n):
+        if n == 0:
+            return "zero"
+        elif n == 1:
+            return "one"
+        elif n == 2:
+            return "two"
+        elif n == 3:
+            return "three"
+        else:
+            return "Invalid number"
+    
+    a_str = number_to_string(a)
+    b_str = number_to_string(b)
+    
+    return a_str, b_str
+
+print(s(1,3))
