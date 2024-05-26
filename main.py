3412550eaa607b84afda502392affe10c346b0d9
karolina <kitanovska.karolina@yahoo.com>
2024-05-27 00:03:42 +0200

    commit 4

--- main.py
+++ main.py
@@ -5,8 +5,4 @@ a = int(input("Enter the first number (0-3): "))
 b = int(input("Enter the second number (0-3): "))
 
 aswaped,bswaped=s(a,b)
-print(f'The numbers swaped are {aswaped}, and {bswaped}')
-
-c=int(input("enter the value of c"))
-cdesc=d(c)
-print(f'c')
+print(f'The numbers swaped are {as
