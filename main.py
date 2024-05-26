d9520d9fa5c2401439359e811bd520a71b478366
karolina <kitanovska.karolina@yahoo.com>
2024-05-26 23:48:07 +0200

    commit 2

---
+++ main.py
@@ -0,0 +1,9 @@
+from swap import s
+
+a = int(input("Enter the first number (0-3): "))
+b = int(input("Enter the second number (0-3): "))
+
+aswaped,bswaped=s(a,b)
+print(f'The numbers swaped are {aswaped}, and {bswaped}')
+
+
